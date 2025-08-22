d073552879031a71ed0395d48fb5b1dc4a82431e
rishabh-75 <srishabh075@gmail.com>
2025-08-22 18:03:43 +0530

    update

--- src/utilities/euler_transform.py
+++ src/utilities/euler_transform.py
@@ -1,11 +1,10 @@
+from annotated_types import LowerCase
 import numpy as np
 
 
 class Euler(object):
-    def __init__(self, options):
-        self.phi = options.get("phi")
-        self.theta = options.get("theta")
-        self.psi = options.get("psi")
+    def __init__(self, eulerAngles):
+        self.eulerAngles = eulerAngles
 
     def radToDeg(self, rad):
         return rad * (180.0 / np.pi)
@@ -14,65 +13,111 @@ class Euler(object):
         return deg * (np.pi / 180.0)
 
     def setEulerAnglesToRadians(self):
-        self.phi = self.degToRad(self.phi)
-        self.theta = self.degToRad(self.theta)
-        self.psi = self.degToRad(self.psi)
+        self.eulerAngles = [self.degToRad(theta) for theta in self.eulerAngles]
 
     def setEulerAnglesToDegrees(self):
-        self.phi = self.radToDeg(self.phi)
-        self.theta = self.radToDeg(self.theta)
-        self.psi = self.radToDeg(self.psi)
+        self.eulerAngles = [self.radToDeg(theta) for theta in self.eulerAngles]
 
-    def rotationX(self):
+    def rotationX(self, theta):
         return np.array(
             [
                 [1, 0, 0],
-                [0, np.cos(self.phi), np.sin(self.phi)],
-                [0, -np.sin(self.phi), np.cos(self.phi)],
+                [0, np.cos(theta), np.sin(theta)],
+                [0, -np.sin(theta), np.cos(theta)],
             ]
         )
 
-    def rotationY(self):
+    def rotationY(self, theta):
         return np.array(
             [
-                [np.cos(self.theta), 0, -np.sin(self.theta)],
+                [np.cos(theta), 0, -np.sin(theta)],
                 [0, 1, 0],
-                [np.sin(self.theta), 0, np.cos(self.theta)],
+                [np.sin(theta), 0, np.cos(theta)],
             ]
         )
 
-    def rotationZ(self):
+    def rotationZ(self, theta):
         return np.array(
             [
-                [np.cos(self.psi), np.sin(self.psi), 0],
-                [-np.sin(self.psi), np.cos(self.psi), 0],
+                [np.cos(theta), np.sin(theta), 0],
+                [-np.sin(theta), np.cos(theta), 0],
                 [0, 0, 1],
             ]
         )
 
-    def rotationEulerXYZ(self):
-        Rx = self.rotationX()
-        Ry = self.rotationY()
-        Rz = self.rotationZ()
-        return np.matmul(Rx, np.matmul(Ry, Rz))
+    def rotationEulerTransformation(self, rotationSequence):
+        """
+        XYZ: Roll | Pitch | Yaw \n
+        ZXZ: Spin | Nutation | Precession \n
 
-    def dcmEulerXYZ(self):
-        dcm = self.rotationEulerXYZ()
-        phi = np.arctan2(dcm[1][2], dcm[2][2])
-        theta = -np.arcsin(dcm[0][2])
-        psi = np.arctan2(dcm[0][1], dcm[0][0])
-        return (phi, theta, psi)
+        !!! rotation right to left
+        """
+        try:
+            if rotationSequence == "XYZ":
+                r3 = self.rotationZ(self.eulerAngles[2])
+                r2 = self.rotationY(self.eulerAngles[1])
+                r1 = self.rotationX(self.eulerAngles[0])
+
+            if rotationSequence == "ZXZ":
+                r3 = self.rotationZ(self.eulerAngles[2])
+                r2 = self.rotationX(self.eulerAngles[1])
+                r1 = self.rotationZ(self.eulerAngles[0])
+
+            return np.matmul(r1, np.matmul(r2, r3))
+        except:
+            print("! invalid sequence")
+
+    def dcmEulerTransformation(self, rotationSequence):
+        try:
+            dcm = self.rotationEulerTransformation(rotationSequence)
+            if rotationSequence == "XYZ":
+                phi = np.arctan2(dcm[1][2], dcm[2][2])
+                theta = -np.arcsin(dcm[0][2])
+                psi = np.arctan2(dcm[0][1], dcm[0][0])
+
+            if rotationSequence == "ZXZ":
+                phi = np.arctan2(dcm[0][2], dcm[1][2])
+                theta = np.arccos(dcm[2][2])
+                psi = np.arctan2(dcm[2][0], -dcm[2][1])
+
+            return (phi, theta, psi)
+        except:
+            print("! invalid sequence")
 
 
-# dcm_options = dict()
-# dcm_options.update(phi=-30.0, theta=65.0, psi=-45.0)
+eulerAngles = [30.0, 65.0, -45.0]
+print(
+    "Euler Angles [{}, {}, {}]".format(
+        eulerAngles[0],
+        eulerAngles[1],
+        eulerAngles[2],
+    )
+)
+eulerTransform = Euler(eulerAngles)
+eulerTransform.setEulerAnglesToRadians()
 
-# print(
-#     "Euler Angles [{}, {}, {}]".format(
-#         dcm_options["phi"], dcm_options["theta"], dcm_options["psi"]
-#     )
-# )
-# eulerTransform = Euler(dcm_options)
-# eulerTransform.setEulerAnglesToRadians()
-# print(eulerTransform.rotationEulerXYZ())
-# print(eulerTransform.dcmEulerXYZ())
+rotationSequence = "XYZ"
+print(
+    f"R{rotationSequence.lower()}: \n",
+    eulerTransform.rotationEulerTransformation(rotationSequence),
+)
+print(
+    f"Recovered {rotationSequence} Euler Angles: ",
+    [
+        eulerTransform.radToDeg(theta)
+        for theta in eulerTransform.dcmEulerTransformation(rotationSequence)
+    ],
+)
+
+rotationSequence = "ZXZ"
+print(
+    f"R{rotationSequence.lower()}: \n",
+    eulerTransform.rotationEulerTransformation(rotationSequence),
+)
+print(
+    f"Recovered {rotationSequence} Euler Angles: ",
+    [
+        eulerTransform.radToDeg(theta)
+        for theta in eulerTransform.dcmEulerTransformation(rotationSequence)
+    ],
+)
